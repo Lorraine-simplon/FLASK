836a3afe9a0f6c3f0243b9c4e04a5c26d8db2bd2
Lorraine FISCHER <lorraine.fischer@hotmail.fr>
2022-01-18 10:52:56 +0100

    test

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template("test.html", message = "Bienvenue")
+def Bienvenue():
+    return render_template("test.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
